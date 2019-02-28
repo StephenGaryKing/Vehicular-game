cde05cd3ac12ec826f4ccbbc7093152a2ca93f27
Unknown <thekings.stephen@gmail.com>
2019-02-28 19:04:45 +1030

    Backup

---
+++ Assets/Scripts/CarCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCamera : MonoBehaviour
+{
+
+	[SerializeField] Transform m_target;
+	[SerializeField] Vector3 m_offset;
+	[SerializeField] float m_followSmoothing = 0.1f;
+	[SerializeField] float m_lookSmoothing = 0.1f;
+
+	void LookAtTarget()
+	{
+		Vector3 lookDir = m_target.position - transform.position;
+		Quaternion rot = Quaternion.LookRotation(lookDir, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, rot, m_lookSmoothing);
+	}
+
+	void MoveToTarget()
+	{
+		Vector3 targetPos = m_target.position +
+							m_target.transform.forward * m_offset.z +
+							m_target.transform.right * m_offset.x +
+							m_target.transform.up * m_offset.y;
+		transform.position = Vector3.Lerp(transform.position, targetPos, m_followSmoothing);
+	}
+
+    void FixedUpdate()
+    {
+		LookAtTarget();
+		MoveToTarget();
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct Wheel
+{
+	public string name;
+	public bool hasMotor;
+	public bool canSteer;
+	public Transform transform;
+	public WheelCollider collider;
+}
+
+public class CarController : MonoBehaviour
+{
+
+	float m_horizontalInput;
+	float m_verticalInput;
+	float m_currentSteeringAngle;
+	float m_currentMotorForce;
+
+	[SerializeField] List<Wheel> m_wheels;
+	[SerializeField] float m_maxSteerAngle = 30;
+	[SerializeField] float m_motorForce = 50;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	void GetInput()
+	{
+		m_horizontalInput = Input.GetAxis("Horizontal");
+		m_verticalInput = Input.GetAxis("Vertical");
+	}
+
+	void ApplyInputData()
+	{
+		m_currentSteeringAngle = m_maxSteerAngle * m_horizontalInput;
+		m_currentMotorForce = m_motorForce * m_verticalInput;
+	}
+
+	void UpdateWheelPos(Wheel wheel)
+	{
+		Vector3 pos = wheel.transform.position;
+		Quaternion rot = wheel.transform.rotation;
+
+		wheel.collider.GetWorldPose(out pos, out rot);
+
+		wheel.transform.position = pos;
+		wheel.transform.rotation = rot;
+	}
+
+	void SteerWheel(Wheel wheel)
+	{
+		if (wheel.canSteer)
+			wheel.collider.steerAngle = m_currentSteeringAngle;
+	}
+
+	void DriveWheel(Wheel wheel)
+	{
+		if (wheel.hasMotor)
+			wheel.collider.motorTorque = m_currentMotorForce;
+	}
+
+	void Update()
+	{
+		GetInput();
+	}
+
+    void FixedUpdate()
+    {
+		ApplyInputData();
+
+		foreach (var wheel in m_wheels)
+		{
+			// Steer wheels
+			SteerWheel(wheel);
+			// Drive wheels
+			DriveWheel(wheel);
+			// Update the visual location of each wheel
+			UpdateWheelPos(wheel);
+		}
+
+	}
+}
